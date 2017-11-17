38c228e6d92bb0632ffef41dcadc77e74648c614
Shannon Enders <shannon.enders.14@cnu.edu>
2017-11-16 21:26:19 -0500

    Angular Shell

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roboTenderGuiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the roboTenderGuiApp
+ */
+angular.module('roboTenderGuiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
